e21a41796f5d02d526d6367b176d7c44b0483abf
rudra990 <rudraashutosh777@gmail.com>
2025-01-24 20:56:44 +0530

    added multiple language support

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from transformers import pipeline
 app = Flask(__name__)
 
 # Load pre-trained sentiment analysis model
-sentiment_pipeline = pipeline("sentiment-analysis")
+sentiment_pipeline = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
 
 from app import routes
